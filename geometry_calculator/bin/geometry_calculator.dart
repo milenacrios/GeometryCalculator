6de19ab6e67d2325970df4cb1e2184aa7a626a6d
milena975 <milena.rios@mail.uft.edu.br>
2021-11-20 10:55:33 -0300

    GeometryCalculator

---
+++ geometry_calculator/bin/geometry_calculator.dart
@@ -0,0 +1,6 @@
+import 'package:geometry_calculator/geometry_calculator.dart'
+    as geometry_calculator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${geometry_calculator.GeometryCalculator}!');
+}
---
+++ geometry_calculator/lib/geometry_calculator.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+class GeometryCalculator {
+  double areaFigura(String tipo, double medidaA, double medidaB) {
+    double area;
+    if (tipo == '1') {
+      //Área Triângulo
+      return area = (medidaA * medidaB) / 2;
+    } else if (tipo == '2') {
+      //Área Quadrado
+      return area = medidaA * medidaB;
+    } else if (tipo == '3') {
+      //Área Retângulo
+      return area = medidaA * medidaB;
+    } else if (tipo == '4') {
+      return area = (medidaA * medidaA) * medidaB;
+    }
+    return 0;
+  }
+}
+
+void main() {
+  String tipoF;
+  double medidaA;
+  double medidaB;
+
+  print("--------------Menu de Opções--------------");
+  print("--------------Digite o tipo da figura geométrica--------------");
+  print("--------------1: Triângulo--------------");
+  print("--------------2: Quadrado--------------");
+  print("--------------3: Retângulo--------------");
+  print("--------------4: Círculo--------------");
+  tipoF = stdin.readLineSync() as String;
+  print(
+      "--------------Digite as duas medidas necessárias para calcular a àrea da figura--------------");
+
+  print("--------------Medida 1:--------------");
+  medidaA = stdin.readLineSync() as double;
+
+  print("--------------Medida 2--------------");
+  medidaB = stdin.readLineSync() as double;
+
+  var result = GeometryCalculator().areaFigura(tipoF, medidaA, medidaB);
+  print("--------------Resultado da área da figura é: $result--------------");
+}
